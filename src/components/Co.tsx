81b25e0ac572b6f2f43a1869d0470cbb8023ae16
Gattp <91740456+Gattp@users.noreply.github.com>
2023-06-28 13:33:11 +0200

    Update Co.tsx

--- src/components/Co.tsx
+++ src/components/Co.tsx
@@ -59,7 +59,7 @@ const Co = ({
         <Content>
           <h3>Ruolo</h3>
           <p>{role}</p>
-          <h3>{pretext ? 'ho fatto' : 'faccio'}</h3>
+          <h3>{pretext ? 'Attività svolta' : 'Attività attuale'}</h3>
           <p>{what}</p>
         </Content>
       </Container>
